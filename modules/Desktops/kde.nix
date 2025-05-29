7cd770a8fdf4d93acef9ef8d428168d6bda78a03
Evan Aceves <evanm.aceves@gmail.com>
2025-05-29 15:06:01 -0700

    refactor: moved desktops to their own directory

---
+++ modules/Desktops/kde.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+{
+	imports = [ ./packages.nix ];
+
+	# ----- X11 and Gnome -----
+	services = {
+		displayManager = {
+			defaultSession = "plasma";
+			sddm = {
+				enable = true;
+				wayland.enable = true;
+			};
+		};
+		desktopManager.plasma6.enable = true;
+	};
+	
+	
+	# ----- QT Configuration -----
+	qt = {
+		enable = true;
+		#platformTheme = "gnome";
+		#style = "adwaita-dark";
+	};
+	
+	# ----- Extra Packages -----
+	environment.systemPackages = with pkgs; [
+		qpwgraph #..... QT-based pipewire manager
+	];
+	
+	# Exclude select default KDE Plasma applications
+	#environment.plasma6.excludePackages = with pkgs.kdePackages; [
+		#plasma-browser-integration
+		#konsole
+		#oxygen
+	#]
+}
