e6533a788589c51718ebd24432fdc6ac3e87972b
Evan Aceves <evanm.aceves@gmail.com>
2026-01-06 15:02:04 -0800

    Added battery section to template hardware.

--- hardware/template.nix
+++ hardware/template.nix
@@ -1,8 +1,69 @@
 
 {lib, pkgs, config, ...}:
-
 {
-	# ----- NVIDIA -----
+	# ------------------------------ BATTERY ------------------------------
+	# - Powertop ........ calibrates to best battery life, but least convenient
+	# - TLP ............. most configurable
+	# - Auto-CPUfreq .... decent?
+	#
+	# Not all configurations work together. Choose between the following
+	# combinations:
+	#
+	# power-profiles daemon | powerManagement | Thermald | TLP | Auto-CPUfreq | Powertop
+	# ----------------------|-----------------|----------|-----|--------------|---------
+	#          x            |        x        |    x     |     |              |
+	# ----------------------|-----------------|----------|-----|--------------|---------
+	#          x            |        x        |    x     |  x  |              |
+	# ----------------------|-----------------|----------|-----|--------------|---------
+	#                       |        x        |    x     |     |      x       |
+	# ----------------------|-----------------|----------|-----|--------------|---------
+	#                       |                 |    x     |     |              |    x
+
+	services.power-profiles-daemon.enable = false;
+	powerManagement.enable = true;
+
+	# ~ Thermald ~
+	services.thermald.enable = true;
+
+	# ~ TLP ~
+	# Note: must disable services.power-profiles-daemon
+	services.tlp = {
+		enable = true;
+		settings = {
+			TLP_DEFAULT_MODE = "BAT";
+			# DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth wifi";
+			# DEVICES_TO_ENABLE_ON_AC = "";
+			# DEVICES_TO_DISABLE_ON_BAT = "bluetooth wifi";
+			# DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth wifi";
+			USB_AUTOSUSPEND = 0;
+			# USB_DENYLIST = "1111:2222 3333:4444";
+			# USB_ALLOWLIST = "1111:2222 3333:4444";
+			# USB_AUTOSUSPEND_DISBALE_ON_SHUTDOWN = 1;  # workaround if suspended USB devices disturb shutdown process
+		};
+	};
+
+	# ~ Auto-CPUfreq ~
+	# Note: do not enable alongside TLP or power-profiles-daemon--choose one.
+	services.auto-cpufreq = {
+		enable = false;
+		settings = {
+			battery = {
+				governor = "powersave";
+				turbo = "never";
+			};
+			charger = {
+				governor = "performance";
+				turbo = "auto";
+			};
+		};
+	};
+
+	# ~ Powertop ~
+	# note: enable by itself. only cooperates with thermald
+	# run powertop --calibrate
+	powerManagement.powertop.enable = false;
+
+	# ------------------------------ NVIDIA ------------------------------
 	# https://wiki.archlinux.org/title/PRIME
 
 	# Enable unfree repository for nvidia-x11, nvidia-settings, nvidia-persistenced
@@ -50,11 +111,11 @@
 			# https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
 			# Only available from driver 515.43.04+
 			# Currently alpha-quality/buggy, so false is currently the recommended setting.
-			#open = false;
+			open = false;
 
 			# Enable the Nvidia settings menu,
 			# accessible via `nvidia-settings`.
-			#nvidiaSettings = true;
+			nvidiaSettings = true;
 
 			# Optionally, you may need to select the appropriate driver version for your specific GPU.
 			package = config.boot.kernelPackages.nvidiaPackages.stable;
@@ -66,7 +127,6 @@
 			#package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
 
 			# ----- Laptop Hybrid Graphics -----
-			open = false;
 			prime = {
 				# Make sure to use the correct Bus ID values for your system!
 				intelBusId  = lib.mkDefault "PCI:0:2:0"; #"PCI:0000:00:02.0";
@@ -107,11 +167,4 @@
 
 	# '';
 	# boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
-
-	# ----- OpenGL -----
-	#hardware.graphics = { #opengl
-		#enable = true;
-		#driSupport = true;
-		#driSupport32Bit = true;
-	#};
 }
