6f03707aeb57299222fadec5830d40fc41f619a4
Evan Aceves <evanm.aceves@gmail.com>
2026-08-11 22:26:40 -0700

    Added option for Jellyfin to not automatically start on boot.

--- modules/jellyfin.nix
+++ modules/jellyfin.nix
@@ -2,7 +2,7 @@
 # HTTP:  8096
 # HTTPS: 8920
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
 	# ----- PACKAGES -----
@@ -23,6 +23,14 @@
 		openFirewall = false;
 		dataDir = "/srv/jellyfin";
 	};
+
+	# The following line clears Jellyfin's "wantedBy" setting for its
+	# systemd service. This means Jellyfin won't start automatically
+	# at boot and must be manually started with the command
+	# 'systemctl start jellyfin.service' . This is handy for when you
+	# don't want to start the service until you manually mount or
+	# decrypt its data directory.
+	#systemd.services.jellyfin.wantedBy = lib.mkForce [];
 
 	# ----- JellySeer (optional) -----
 	#services.jellyseer = {
