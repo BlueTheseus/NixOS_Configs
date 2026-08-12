aceb35f1ef40bfafb904c3cea2409c13b2fc9146
Evan Aceves <evanm.aceves@gmail.com>
2026-08-11 22:24:15 -0700

    Updated Gnome desktop for current NixOS version.

--- modules/Desktops/gnome.nix
+++ modules/Desktops/gnome.nix
@@ -2,29 +2,58 @@
 
 {
 	# ----- X11 and Gnome -----
-	services.xserver.enable = true;
-	services.xserver.displayManager.gdm.enable = true;
-	#services.xserver.displayManager.gdm.autoSuspend = false;  # Auto-suspend prevents connecting as a server
-	services.xserver.desktopManager.gnome.enable = true;
+	#services.xserver.enable = true;
+	services.displayManager.gdm = {
+		enable = true;
+		#autoSuspend = false; # Auto-suspend prevents connecting as a server
+	};
+	services.desktopManager.gnome.enable = true;
+
 	# The following steps may also be required to fully disable auto-suspend:
 	# dconf write /org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-timeout 0
 	# dconf write /org/gnome/settings-daemon/plugins/power/sleep-inactive-battery-timeout 0
 	# restart gdm (kill process or reboot)
 
 
+	#services.gnome = {
+		#core-apps.enable = false;
+		#core-developer-tools.enable = false;
+		#games.enable = false;
+	#};
+
+	# ----- Remote Desktop -----
+	#services.gnome.gnome-remote-desktop.enable = true;
+
+	# Ensure service starts automatically at boot so the settings panel appears
+	#systemd.services.gnome-remote-desktop = {
+	#	wantedBy = [ "graphical.target" ];
+	#};
+
+	# Disable autologin to avoid session conflicts
+	#services.displayManager.autoLogin.enable = false;
+	#services.getty.autologinUser = null;
+
+	# Prevent automatic suspend
+	#systemd.targets.sleep.enable = false;
+	#systemd.targets.suspend.enable = false;
+	#systemd.targets.hibernate.enable = false;
+	#systemd.targets.hybrid-sleep.enable = false;
+
+
 	# ----- Extra Packages -----
 	environment.systemPackages = with pkgs; [
-		helvum #.... Graphical pipewire manager
+		#helvum #............................. Graphical pipewire manager
+		crosspipe #........................... Graphical pipewire manager
 		gnome-solanum
 		# Gnome-Specific
-		gnomeExtensions.appindicator  # extension for system tray icons
+		gnomeExtensions.appindicator  #....... extension for system tray icons
 		gnomeExtensions.pop-shell
 		gnomeExtensions.tailscale-status
 	];
 
 	# Exclude select default Gnome applications
 	environment.gnome.excludePackages = (with pkgs; [
-		gnome-connections
+		#gnome-connections
 		#gnome-photos
 		gnome-tour
 		gedit  # graphical text editor
